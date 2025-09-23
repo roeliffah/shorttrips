e854d98586184c8085cc3a51992b07100b04e888
roeliffah <roeliffah@gmail.com>
2025-09-23 12:46:12 +0000

    Toon WordPress-site fullscreen via iframe in React-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 
 function App() {
   return (
-    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
+    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0, overflow: "hidden" }}>
       <iframe
         src="https://shorttrips.eu/"
         title="Shorttrips WordPress Frontend"
-        style={{ width: "100%", height: "100%", border: "none" }}
+        style={{ width: "100vw", height: "100vh", border: "none", margin: 0, padding: 0, display: "block" }}
+        allowFullScreen
       />
     </div>
   );
