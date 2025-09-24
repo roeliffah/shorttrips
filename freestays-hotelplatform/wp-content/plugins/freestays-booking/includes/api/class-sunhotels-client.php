7a0b998680c7ab7eae817065dafadd569e3f6385
roeliffah <roeliffah@gmail.com>
2025-09-24 11:47:29 +0000

    Update Sunhotels client en zoekshortcode voor correcte parameter mapping en 1 kamer per boeking

--- freestays-hotelplatform/wp-content/plugins/freestays-booking/includes/api/class-sunhotels-client.php
+++ freestays-hotelplatform/wp-content/plugins/freestays-booking/includes/api/class-sunhotels-client.php
@@ -3,89 +3,271 @@ class Sunhotels_Client {
     private $apiUrl;
     private $apiUser;
     private $apiPass;
+    private $language;
+    private $currency;
+    private $customerCountry;
 
-    public function __construct($apiUrl, $apiUser, $apiPass) {
-        $this->apiUrl = $apiUrl;
-        $this->apiUser = $apiUser;
-        $this->apiPass = $apiPass;
+    public function __construct($apiUrl, $apiUser, $apiPass, $language = 'en', $currency = 'EUR', $customerCountry = 'NL') {
+        $this->apiUrl         = rtrim($apiUrl, '/');
+        $this->apiUser        = $apiUser;
+        $this->apiPass        = $apiPass;
+        $this->language       = $language;
+        $this->currency       = $currency;
+        $this->customerCountry = $customerCountry;
     }
 
-    public function searchHotels($params) {
-        $xml = $this->buildSearchXml($params);
-        $response = $this->sendRequest($xml);
-        return $this->parseResponse($response);
-    }
+    /**
+     * Zoek hotels via de Sunhotels API SearchV2 (NonStatic).
+     * @param string $destination IATA-code of destination (bijv. PMI)
+     * @param string $checkin (YYYY-MM-DD)
+     * @param string $checkout (YYYY-MM-DD)
+     * @param int $adults
+     * @param int $children
+     * @param array $child_ages
+     * @param int $infant
+     * @return array
+     * @throws Exception
+     */
+    public function searchHotels($destination, $checkin, $checkout, $adults, $children, $child_ages, $infant = 0) {
+        $endpoint = $this->apiUrl . '/SearchV2';
 
-    private function buildSearchXml($params) {
-        $checkin = htmlspecialchars($params['checkin']);
-        $checkout = htmlspecialchars($params['checkout']);
-        $adults = intval($params['adults'] ?? 2);
-        $children = intval($params['children'] ?? 0);
-        $rooms = intval($params['rooms'] ?? 1);
-        $destinationId = intval($params['destination_id'] ?? 0);
-        $hotelId = htmlspecialchars($params['hotel_id'] ?? '');
+        // Sunhotels ondersteunt alleen 1 kamer per boeking
+        $numberOfRooms = 1;
 
-        $destinationBlock = $hotelId ? "<hotelIDs>$hotelId</hotelIDs>" : "<destinationID>$destinationId</destinationID>";
+        $childrenAges = ($children > 0 && !empty($child_ages)) ? implode(',', $child_ages) : '';
 
-        return '<?xml version="1.0" encoding="utf-8"?>' .
-            '<SearchV3 xmlns="http://xml.sunhotels.net/15/">' .
-            '<userName>' . $this->apiUser . '</userName>' .
-            '<password>' . $this->apiPass . '</password>' .
-            '<language>EN</language>' .
-            '<currencies>EUR</currencies>' .
-            '<searchV3Request>' .
-            '<checkInDate>' . $checkin . '</checkInDate>' .
-            '<checkOutDate>' . $checkout . '</checkOutDate>' .
-            '<numberOfRooms>' . $rooms . '</numberOfRooms>' .
-            $destinationBlock .
-            '<blockSuperdeal>ja</blockSuperdeal>' .
-            '<paxRooms>' .
-            str_repeat('<paxRoom><numberOfAdults>' . $adults . '</numberOfAdults><numberOfChildren>' . $children . '</numberOfChildren></paxRoom>', $rooms) .
-            '</paxRooms>' .
-            '</searchV3Request>' .
-            '</SearchV3>';
-    }
+        $params = [
+            'userName'         => $this->apiUser,
+            'password'         => $this->apiPass,
+            'language'         => $this->language,
+            'currencies'       => $this->currency,
+            'checkInDate'      => $checkin,
+            'checkOutDate'     => $checkout,
+            'numberOfRooms'    => $numberOfRooms,
+            'destination'      => $destination,
+            'numberOfAdults'   => $adults,
+            'numberOfChildren' => $children,
+            'childrenAges'     => $childrenAges,
+            'infant'           => $infant,
+            'customerCountry'  => $this->customerCountry,
+            'B2C'              => 0, // Altijd 0
+        ];
 
-    private function sendRequest($xml) {
-        $ch = curl_init($this->apiUrl);
-        curl_setopt_array($ch, [
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HTTPHEADER => [
-                "Content-Type: text/xml; charset=utf-8",
-                "SOAPAction: http://xml.sunhotels.net/15/SearchV3",
+        $url = $endpoint . '?' . http_build_query($params);
+
+        $response = wp_remote_get($url, [
+            'timeout' => 30,
+            'headers' => [
+                'Accept' => 'application/xml',
             ],
-            CURLOPT_POST => true,
-            CURLOPT_POSTFIELDS => $xml,
-            CURLOPT_TIMEOUT => 30,
         ]);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        return $response;
-    }
 
-    private function parseResponse($response) {
-        libxml_use_internal_errors(true);
-        $parsed = simplexml_load_string($response);
-        if (!$parsed) {
-            return ['error' => 'Invalid response from Sunhotels API'];
+        if (is_wp_error($response)) {
+            throw new Exception('API niet bereikbaar: ' . $response->get_error_message());
+        }
+
+        $body = wp_remote_retrieve_body($response);
+        if (empty($body)) {
+            throw new Exception('Lege response van Sunhotels API.');
+        }
+
+        $xml = simplexml_load_string($body, 'SimpleXMLElement', LIBXML_NOCDATA);
+        if ($xml === false) {
+            throw new Exception('Ongeldige XML van Sunhotels API.');
+        }
+
+        if (isset($xml->Error)) {
+            $errorType = (string)($xml->Error->ErrorType ?? '');
+            $errorMsg  = (string)($xml->Error->Message ?? 'Onbekende fout');
+            throw new Exception("Sunhotels API error: $errorType - $errorMsg");
         }
 
         $hotels = [];
-        foreach ($parsed->searchV3Response->hotels->hotel as $hotel) {
-            $hotels[] = [
-                'hotel_id' => (string)$hotel['id'],
-                'name' => (string)$hotel->name,
-                'city' => (string)$hotel->city,
-                'country' => (string)$hotel->country,
-                'star_rating' => (string)$hotel->starRating,
-                'address' => (string)$hotel->address,
-                'image_url' => (string)$hotel->mainImage,
-                'availability' => true,
-                'price_total' => (string)$hotel->rooms->room->price->total,
-                'currency' => (string)$hotel->rooms->room->price->currency,
-            ];
+        if (isset($xml->hotels->hotel)) {
+            foreach ($xml->hotels->hotel as $hotel) {
+                $hotelArr = [
+                    'id'           => (string)($hotel->{'hotel.id'} ?? ''),
+                    'name'         => (string)($hotel->name ?? ''),
+                    'destination'  => (string)($hotel->destination ?? ''),
+                    'city'         => (string)($hotel->{'hotel.addr.city'} ?? ''),
+                    'address'      => (string)($hotel->{'hotel.address'} ?? ''),
+                    'classification' => (string)($hotel->classification ?? ''),
+                    'roomtypes'    => [],
+                ];
+
+                if (isset($hotel->roomtypes->roomtype)) {
+                    foreach ($hotel->roomtypes->roomtype as $roomtype) {
+                        $roomtypeArr = [
+                            'id'   => (string)($roomtype->{'roomtype.ID'} ?? ''),
+                            'name' => (string)($roomtype->{'roomtype.Name'} ?? ''),
+                            'rooms' => [],
+                        ];
+                        if (isset($roomtype->rooms->room)) {
+                            foreach ($roomtype->rooms->room as $room) {
+                                $roomArr = [
+                                    'id'        => (string)($room->id ?? ''),
+                                    'beds'      => (string)($room->beds ?? ''),
+                                    'extrabeds' => (string)($room->extrabeds ?? ''),
+                                    'meals'     => [],
+                                    'prices'    => [],
+                                ];
+                                if (isset($room->meals->meal)) {
+                                    foreach ($room->meals->meal as $meal) {
+                                        $mealArr = [
+                                            'id'       => (string)($meal->id ?? ''),
+                                            'labelId'  => (string)($meal->labelId ?? ''),
+                                            'name'     => (string)($meal->name ?? ''),
+                                            'labelText'=> (string)($meal->labelText ?? ''),
+                                            'prices'   => [],
+                                        ];
+                                        if (isset($meal->prices->price)) {
+                                            foreach ($meal->prices->price as $price) {
+                                                $mealArr['prices'][] = [
+                                                    'currency'        => (string)$price['currency'],
+                                                    'paymentMethods'  => (string)$price['paymentMethods'],
+                                                    'amount'          => (string)$price,
+                                                ];
+                                            }
+                                        }
+                                        $roomArr['meals'][] = $mealArr;
+                                    }
+                                }
+                                if (isset($room->prices->price)) {
+                                    foreach ($room->prices->price as $price) {
+                                        $roomArr['prices'][] = [
+                                            'currency'        => (string)$price['currency'],
+                                            'paymentMethods'  => (string)$price['paymentMethods'],
+                                            'amount'          => (string)$price,
+                                        ];
+                                    }
+                                }
+                                $roomtypeArr['rooms'][] = $roomArr;
+                            }
+                        }
+                        $hotelArr['roomtypes'][] = $roomtypeArr;
+                    }
+                }
+                $hotels[] = $hotelArr;
+            }
         }
 
-        return ['hotels' => $hotels];
+        return $hotels;
+    }
+
+    /**
+     * PreBook-aanvraag bij Sunhotels API.
+     * @param array $prebookData Sunhotels parameters, zie documentatie
+     * @return array
+     * @throws Exception
+     */
+    public function preBook($prebookData) {
+        $endpoint = $this->apiUrl . '/PreBookV2';
+
+        // Sunhotels ondersteunt alleen 1 kamer per boeking
+        $prebookData['numberOfRooms'] = 1;
+
+        $params = array_merge([
+            'userName'        => $this->apiUser,
+            'password'        => $this->apiPass,
+            'language'        => $this->language,
+            'currencies'      => $this->currency,
+            'customerCountry' => $this->customerCountry,
+            'B2C'             => 0, // Altijd 0
+        ], $prebookData);
+
+        $url = $endpoint . '?' . http_build_query($params);
+
+        $response = wp_remote_get($url, [
+            'timeout' => 30,
+            'headers' => [
+                'Accept' => 'application/xml',
+            ],
+        ]);
+
+        if (is_wp_error($response)) {
+            throw new Exception('API niet bereikbaar: ' . $response->get_error_message());
+        }
+
+        $body = wp_remote_retrieve_body($response);
+        if (empty($body)) {
+            throw new Exception('Lege response van Sunhotels API (PreBook).');
+        }
+
+        $xml = simplexml_load_string($body, 'SimpleXMLElement', LIBXML_NOCDATA);
+        if ($xml === false) {
+            throw new Exception('Ongeldige XML van Sunhotels API (PreBook).');
+        }
+
+        if (isset($xml->Error)) {
+            $errorType = (string)($xml->Error->ErrorType ?? '');
+            $errorMsg  = (string)($xml->Error->Message ?? 'Onbekende fout');
+            throw new Exception("Sunhotels API PreBook error: $errorType - $errorMsg");
+        }
+
+        return json_decode(json_encode($xml), true);
+    }
+
+    /**
+     * Book-aanvraag bij Sunhotels API.
+     * @param array $bookData Sunhotels parameters, zie documentatie
+     * @param bool $paymentCompleted (vereist: true als betaling is afgerond)
+     * @return array
+     * @throws Exception
+     */
+    public function book($bookData, $paymentCompleted = false) {
+        // Alleen boeken als betaling is afgerond
+        if (!$paymentCompleted) {
+            throw new Exception('Boeking mag pas naar Sunhotels na succesvolle betaling.');
+        }
+
+        // Alleen echte boekingen toestaan als userName NIET FreestaysTEST is
+        if ($this->apiUser === 'FreestaysTEST') {
+            throw new Exception('Boeken is niet toegestaan in testmodus (FreestaysTEST).');
+        }
+
+        $endpoint = $this->apiUrl . '/BookV2';
+
+        // Sunhotels ondersteunt alleen 1 kamer per boeking
+        $bookData['numberOfRooms'] = 1;
+
+        $params = array_merge([
+            'userName'        => $this->apiUser,
+            'password'        => $this->apiPass,
+            'language'        => $this->language,
+            'currencies'      => $this->currency,
+            'customerCountry' => $this->customerCountry,
+            'B2C'             => 0, // Altijd 0
+        ], $bookData);
+
+        $url = $endpoint . '?' . http_build_query($params);
+
+        $response = wp_remote_get($url, [
+            'timeout' => 30,
+            'headers' => [
+                'Accept' => 'application/xml',
+            ],
+        ]);
+
+        if (is_wp_error($response)) {
+            throw new Exception('API niet bereikbaar: ' . $response->get_error_message());
+        }
+
+        $body = wp_remote_retrieve_body($response);
+        if (empty($body)) {
+            throw new Exception('Lege response van Sunhotels API (Book).');
+        }
+
+        $xml = simplexml_load_string($body, 'SimpleXMLElement', LIBXML_NOCDATA);
+        if ($xml === false) {
+            throw new Exception('Ongeldige XML van Sunhotels API (Book).');
+        }
+
+        if (isset($xml->Error)) {
+            $errorType = (string)($xml->Error->ErrorType ?? '');
+            $errorMsg  = (string)($xml->Error->Message ?? 'Onbekende fout');
+            throw new Exception("Sunhotels API Book error: $errorType - $errorMsg");
+        }
+
+        return json_decode(json_encode($xml), true);
     }
 }
--- freestays-hotelplatform/wp-content/plugins/freestays-booking/includes/shortcodes/class-freestays-search-shortcode.php
+++ freestays-hotelplatform/wp-content/plugins/freestays-booking/includes/shortcodes/class-freestays-search-shortcode.php
@@ -9,12 +9,16 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 class Freestays_Search_Shortcode {
     public static function register() {
         add_shortcode( 'freestays_search', [ __CLASS__, 'render' ] );
+        // AJAX handlers registreren
+        add_action( 'wp_ajax_freestays_search', [ __CLASS__, 'handle_search_form' ] );
+        add_action( 'wp_ajax_nopriv_freestays_search', [ __CLASS__, 'handle_search_form' ] );
     }
 
     public static function render( $atts = [], $content = null ) {
         ob_start();
         ?>
-        <form id="freestays-search-form" class="freestays-search-form" method="get" action="">
+        <form id="freestays-search-form" class="freestays-search-form" method="post" action="<?php echo admin_url('admin-ajax.php'); ?>">
+            <input type="hidden" name="action" value="freestays_search" />
             <div class="fs-search-row">
                 <input type="text" name="fs_query" placeholder="Hotel, regio of land" required />
             </div>
@@ -33,7 +37,8 @@ class Freestays_Search_Shortcode {
             </div>
             <div class="fs-search-row">
                 <label>Kamers</label>
-                <input type="number" name="fs_rooms" min="1" value="1" required />
+                <input type="number" name="fs_rooms" min="1" value="1" required disabled />
+                <small style="color:#888;">Sunhotels ondersteunt alleen 1 kamer per boeking</small>
             </div>
             <div class="fs-search-row">
                 <button type="submit" class="fs-btn fs-btn-primary">Zoeken</button>
@@ -64,9 +69,92 @@ class Freestays_Search_Shortcode {
             }
             childrenInput.addEventListener('input', updateAges);
             updateAges();
+
+            // AJAX submit
+            document.getElementById('freestays-search-form').addEventListener('submit', function(e) {
+                e.preventDefault();
+                const form = e.target;
+                const formData = new FormData(form);
+                fetch(form.action, {
+                    method: 'POST',
+                    body: formData,
+                    credentials: 'same-origin'
+                })
+                .then(response => response.json())
+                .then(data => {
+                    const resultsDiv = document.getElementById('freestays-search-results');
+                    if (data.success) {
+                        // Toon resultaten als cards (voorbeeld)
+                        if (Array.isArray(data.data) && data.data.length > 0) {
+                            resultsDiv.innerHTML = data.data.map(hotel => `
+                                <div class="fs-hotel-card">
+                                    <h3>${hotel.name}</h3>
+                                    <p>${hotel.address}, ${hotel.city}</p>
+                                    <p>Sterren: ${hotel.classification}</p>
+                                </div>
+                            `).join('');
+                        } else {
+                            resultsDiv.innerHTML = '<p>Geen hotels gevonden.</p>';
+                        }
+                    } else {
+                        resultsDiv.innerHTML = '<p style="color:red;">' + (data.data && data.data.message ? data.data.message : 'Er is een fout opgetreden.') + '</p>';
+                    }
+                })
+                .catch(() => {
+                    document.getElementById('freestays-search-results').innerHTML = '<p style="color:red;">Er is een fout opgetreden bij het zoeken.</p>';
+                });
+            });
         });
         </script>
+        <style>
+        .fs-hotel-card { border:1px solid #ddd; padding:15px; margin-bottom:15px; border-radius:4px; }
+        </style>
         <?php
         return ob_get_clean();
+    }
+
+    public static function handle_search_form() {
+        // Mapping van formulier POST naar Sunhotels parameters
+        $destination    = isset($_POST['fs_query']) ? sanitize_text_field($_POST['fs_query']) : '';
+        $checkin        = isset($_POST['fs_checkin']) ? sanitize_text_field($_POST['fs_checkin']) : '';
+        $checkout       = isset($_POST['fs_checkout']) ? sanitize_text_field($_POST['fs_checkout']) : '';
+        $adults         = isset($_POST['fs_adults']) ? intval($_POST['fs_adults']) : 2;
+        $children       = isset($_POST['fs_children']) ? intval($_POST['fs_children']) : 0;
+        $infant         = isset($_POST['fs_infant']) ? intval($_POST['fs_infant']) : 0;
+
+        // Verzamel kind-leeftijden
+        $child_ages = [];
+        for ($i = 1; $i <= $children; $i++) {
+            $key = 'fs_child_age_' . $i;
+            if (isset($_POST[$key])) {
+                $child_ages[] = intval($_POST[$key]);
+            }
+        }
+
+        // Haal credentials uit je config/.env
+        $apiUrl         = getenv('API_URL');
+        $apiUser        = getenv('API_USER');
+        $apiPass        = getenv('API_PASS');
+        $language       = 'nl';
+        $currency       = 'EUR';
+        $customerCountry = 'NL';
+
+        $client = new Sunhotels_Client($apiUrl, $apiUser, $apiPass, $language, $currency, $customerCountry);
+
+        try {
+            $hotels = $client->searchHotels(
+                $destination,
+                $checkin,
+                $checkout,
+                $adults,
+                $children,
+                $child_ages,
+                $infant
+            );
+            wp_send_json_success($hotels);
+        } catch (Exception $e) {
+            wp_send_json_error(['message' => $e->getMessage()]);
+        }
+        wp_die();
     }
 }
