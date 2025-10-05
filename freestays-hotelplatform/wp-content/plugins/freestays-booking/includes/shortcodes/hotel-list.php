c6d670c9d92b54c1d2fb9831eda48f0a0e875658
roeliffah <roeliffah@gmail.com>
2025-10-05 15:41:27 +0000

    Bridge API Laravel controller toegevoegd en plugin aangepast

--- freestays-hotelplatform/wp-content/plugins/freestays-booking/includes/shortcodes/hotel-list.php
+++ freestays-hotelplatform/wp-content/plugins/freestays-booking/includes/shortcodes/hotel-list.php
@@ -9,30 +9,35 @@ function freestays_hotel_list_shortcode($atts) {
         'rooms' => 1,
     ], $atts);
 
-    $api_url  = $_ENV['API_URL'] ?? getenv('API_URL') ?? '';
-    $api_user = $_ENV['API_USER'] ?? getenv('API_USER') ?? '';
-    $api_pass = $_ENV['API_PASS'] ?? getenv('API_PASS') ?? '';
-
     require_once dirname(__DIR__) . '/api/class-sunhotels-client.php';
-    $client = new Sunhotels_Client($api_url, $api_user, $api_pass);
+    $client = new Sunhotels_Client();
 
     try {
-        $hotels = $client->searchHotels(
-            '', '', '', $atts['destination_id'],
-            $atts['checkin'], $atts['checkout'],
-            $atts['adults'], $atts['children'], $atts['rooms']
-        );
+        $args = [
+            'start' => $atts['checkin'],
+            'end' => $atts['checkout'],
+            'room' => $atts['rooms'],
+            'adults' => $atts['adults'],
+            'children' => $atts['children'],
+            'destination_id' => $atts['destination_id'],
+        ];
+        $results = $client->searchV3($args);
     } catch (Exception $e) {
         return '<div class="error">Fout bij ophalen hotels: ' . esc_html($e->getMessage()) . '</div>';
     }
 
     ob_start();
     echo '<div class="freestays-hotel-list">';
-    foreach ($hotels as $hotel) {
-        $hotel_data = $hotel; // Zorg dat dit overeenkomt met je hotel-card.php
-        include dirname(__DIR__) . '/../templates/hotel-card.php';
+    if (!empty($results['hotels'])) {
+        foreach ($results['hotels'] as $hotel) {
+            $hotel_data = $hotel; // Zorg dat dit overeenkomt met je hotel-card.php
+            include dirname(__DIR__) . '/../templates/hotel-card.php';
+        }
+    } else {
+        echo '<div class="no-results">Geen hotels gevonden.</div>';
     }
     echo '</div>';
     return ob_get_clean();
 }
 add_shortcode('freestays_hotel_list', 'freestays_hotel_list_shortcode');
+?>
