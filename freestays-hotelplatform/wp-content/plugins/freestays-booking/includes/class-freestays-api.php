be5c99a99233f5853f04371f456c883ad48b8467
roeliffah <roeliffah@gmail.com>
2025-10-09 21:29:38 +0000

    Debug logging toegevoegd aan REST endpoints voor Sunhotels API

--- freestays-hotelplatform/wp-content/plugins/freestays-booking/includes/class-freestays-api.php
+++ freestays-hotelplatform/wp-content/plugins/freestays-booking/includes/class-freestays-api.php
@@ -7,46 +7,52 @@ class Freestays_API {
         register_rest_route('freestays/v1', '/countries', [
             'methods' => 'GET',
             'callback' => [self::class, 'get_countries'],
-            'permission_callback' => '__return_true',
+              'permission_callback' => '__return_true',
         ]);
         register_rest_route('freestays/v1', '/cities', [
             'methods' => 'GET',
             'callback' => [self::class, 'get_cities'],
-            'permission_callback' => '__return_true',
+              'permission_callback' => '__return_true',
         ]);
         register_rest_route('freestays/v1', '/resorts', [
             'methods' => 'GET',
             'callback' => [self::class, 'get_resorts'],
-            'permission_callback' => '__return_true',
+              'permission_callback' => '__return_true',
         ]);
         register_rest_route('freestays/v1', '/search', [
             'methods' => 'POST',
             'callback' => [self::class, 'search_hotels'],
-            'permission_callback' => '__return_true',
+              'permission_callback' => '__return_true',
         ]);
     }
 
     public static function get_countries($request) {
         require_once __DIR__ . '/api/class-sunhotels-client.php';
         $client = new Sunhotels_Client();
-        $countries = $client->getCountries();
-        return ['success' => true, 'data' => $countries];
+        error_log('[freestays] get_countries aangeroepen');
+        $result = $client->getCountries();
+        error_log('[freestays] get_countries resultaat: ' . print_r($result, true));
+        return rest_ensure_response(['data' => $result]);
     }
 
     public static function get_cities($request) {
         $country_id = $request->get_param('country_id');
         require_once __DIR__ . '/api/class-sunhotels-client.php';
         $client = new Sunhotels_Client();
-        $cities = $client->getCities($country_id);
-        return ['success' => true, 'data' => $cities];
+        error_log('[freestays] get_cities aangeroepen met country_id: ' . $country_id);
+        $result = $client->getCities($country_id);
+        error_log('[freestays] get_cities resultaat: ' . print_r($result, true));
+        return rest_ensure_response(['data' => $result]);
     }
 
     public static function get_resorts($request) {
         $city_id = $request->get_param('city_id');
         require_once __DIR__ . '/api/class-sunhotels-client.php';
         $client = new Sunhotels_Client();
-        $resorts = $client->getResorts($city_id);
-        return ['success' => true, 'data' => $resorts];
+        error_log('[freestays] get_resorts aangeroepen met city_id: ' . $city_id);
+        $result = $client->getResorts($city_id);
+        error_log('[freestays] get_resorts resultaat: ' . print_r($result, true));
+        return rest_ensure_response(['data' => $result]);
     }
 
     public static function search_hotels($request) {
